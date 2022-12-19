4aa56bd6bb1bc6b2366374309fca95985958ee29
jeermuce <93339647+jeermuce@users.noreply.github.com>
2022-12-19 13:48:13 -0600

    .

--- exercises/error_handling/errors6.rs
+++ exercises/error_handling/errors6.rs
@@ -12,35 +12,32 @@
 
 use std::num::ParseIntError;
 
-// This is a custom error type that we will be using in `PPN()`.
+// This is a custom error type that we will be using in `parse_pos_nonzero()`.
 #[derive(PartialEq, Debug)]
-enum PPNerror {
+enum ParsePosNonzeroError {
     Creation(CreationError),
     ParseInt(ParseIntError),
 }
 
-impl PPNerror {
-    fn from_creation(err: CreationError) -> PPNerror {
-        PPNerror::Creation(err)
+impl ParsePosNonzeroError {
+    fn from_creation(err: CreationError) -> ParsePosNonzeroError {
+        ParsePosNonzeroError::Creation(err)
     }
     // TODO: add another error conversion function here.
-    fn from_parseint(){
-        PPNerror::ParseInt(err)
-
-    }
+    // fn from_parseint...
 }
 
-fn PPN(s: &str) -> Result<PNZinteger, PPNerror> {
+fn parse_pos_nonzero(s: &str) -> Result<PositiveNonzeroInteger, ParsePosNonzeroError> {
     // TODO: change this to return an appropriate error instead of panicking
     // when `parse()` returns an error.
     let x: i64 = s.parse().unwrap();
-    PNZinteger::new(x).map_err(PPNerror::from_creation)
+    PositiveNonzeroInteger::new(x).map_err(ParsePosNonzeroError::from_creation)
 }
 
 // Don't change anything below this line.
 
 #[derive(PartialEq, Debug)]
-struct PNZinteger(u64);
+struct PositiveNonzeroInteger(u64);
 
 #[derive(PartialEq, Debug)]
 enum CreationError {
@@ -48,12 +45,12 @@ enum CreationError {
     Zero,
 }
 
-impl PNZinteger {
-    fn new(value: i64) -> Result<PNZinteger, CreationError> {
+impl PositiveNonzeroInteger {
+    fn new(value: i64) -> Result<PositiveNonzeroInteger, CreationError> {
         match value {
             x if x < 0 => Err(CreationError::Negative),
             x if x == 0 => Err(CreationError::Zero),
-            x => Ok(PNZinteger(x as u64)),
+            x => Ok(PositiveNonzeroInteger(x as u64)),
         }
     }
 }
@@ -66,31 +63,31 @@ mod test {
     fn test_parse_error() {
         // We can't construct a ParseIntError, so we have to pattern match.
         assert!(matches!(
-            PPN("not a number"),
-            Err(PPNerror::ParseInt(_))
+            parse_pos_nonzero("not a number"),
+            Err(ParsePosNonzeroError::ParseInt(_))
         ));
     }
 
     #[test]
     fn test_negative() {
         assert_eq!(
-            PPN("-555"),
-            Err(PPNerror::Creation(CreationError::Negative))
+            parse_pos_nonzero("-555"),
+            Err(ParsePosNonzeroError::Creation(CreationError::Negative))
         );
     }
 
     #[test]
     fn test_zero() {
         assert_eq!(
-            PPN("0"),
-            Err(PPNerror::Creation(CreationError::Zero))
+            parse_pos_nonzero("0"),
+            Err(ParsePosNonzeroError::Creation(CreationError::Zero))
         );
     }
 
     #[test]
     fn test_positive() {
-        let x = PNZinteger::new(42);
+        let x = PositiveNonzeroInteger::new(42);
         assert!(x.is_ok());
-        assert_eq!(PPN("42"), Ok(x.unwrap()));
+        assert_eq!(parse_pos_nonzero("42"), Ok(x.unwrap()));
     }
 }
