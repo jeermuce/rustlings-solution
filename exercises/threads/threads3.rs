77485c57d12fa519f45fcc92fe05b86437f753f7
jeermuce <93339647+jeermuce@users.noreply.github.com>
2022-12-26 13:05:34 -0600

    done

--- exercises/threads/threads3.rs
+++ exercises/threads/threads3.rs
@@ -28,6 +28,8 @@ fn send_tx(q: Queue, tx: mpsc::Sender<u32>) -> () {
     let qc = Arc::new(q);
     let qc1 = Arc::clone(&qc);
     let qc2 = Arc::clone(&qc);
+    let tx1 = tx.clone();
+    let
 
     thread::spawn(move || {
         for val in &qc1.first_half {
